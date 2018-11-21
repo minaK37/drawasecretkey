01f06049f605096bc8bdcf71f2628ab17254765a
minak37 <37lunkhead91@gmail.com>
2018-11-21 17:09:09 +0900

    修正

--- server.js
+++ server.js
@@ -1,30 +1,11 @@
 const express = require("express");
-//const http = require('http');
-//const hostname = '127.0.0.1';
+
 const port = process.env.PORT || 8080;
 
 const app = express();
 
-//var server = http.createServer();
-//server.on('request', doRequest);
 
-// ファイルモジュールを読み込む
 var fs = require('fs');
-
-// リクエストの処理
-// function doRequest(req, res) {
-//
-//     // ファイルを読み込んだら、コールバック関数を実行する。
-//     fs.readFile('./index.html', 'utf-8' , doReard );
-//
-//     // コンテンツを表示する。
-//     function doReard(err, data) {
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         res.write(data);
-//         res.end();
-//     }
-//
-// }
 
 app.use(express.static("public"));
 app.get("/",function(req,res){
